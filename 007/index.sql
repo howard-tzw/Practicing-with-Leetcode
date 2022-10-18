f54a22e63d3282390ca1ad3f4d6da5b83e119d8d
Howard <howard.tzw@gmail.com>
2022-10-18 10:42:31 +0800

    day 7

--- 007/index.sql
+++ 007/index.sql
@@ -1,1 +1,8 @@
 -- https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/
+-- group by 可以用兩個以上的欄位，符合所有欄位值皆相同資料才會被分為一組
+-- 同上一題要運用 count(*)
+
+select actor_id, director_id 
+from ActorDirector
+group by actor_id, director_id
+having count(*) >= 3;
--- 007/main.py
+++ 007/main.py
@@ -1,1 +1,33 @@
 # https://ithelp.ithome.com.tw/articles/10278179
+
+# 分割：分割含有 n 個資料需要 n-1 次，O(n)。
+# 合併：合併的兩邊共用 n 個元素，每次都是比較最左邊的資料，將較小的加到新陣列中，因此每次排序與合併必須經過 n 次，每回合log n次，O(log n)。
+# 平均時間複雜度為: O(n log n)
+
+data = [89, 34, 23, 78, 67, 100, 66, 29, 79, 55, 78, 88, 92, 96, 96, 23]
+
+
+def merge(left, right):    # 合併
+    result = []
+    while len(left) and len(right):
+        if left[0] < right[0]:
+            result.append(left.pop(0))
+        else:
+            result.append(right.pop(0))
+
+    result = result+left if len(left) else result+right
+    return result
+
+
+def merge_sort(arr):    # 分割
+    if len(arr) < 2:
+        return arr
+
+    mid = len(arr)//2
+    leftArray = arr[:mid]
+    rightArray = arr[mid:]
+
+    return merge(merge_sort(leftArray), merge_sort(rightArray))
+
+
+print(merge_sort(data))
