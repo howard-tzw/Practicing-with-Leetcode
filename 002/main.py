a14ec3a485694a98df61335eb373ed167dc9be5a
Johnson <chnejohnson@gmail.com>
2022-11-02 20:43:27 +0800

    restart

--- 002/main.py
+++ 002/main.py
@@ -1,17 +1,18 @@
-
+# https://ithelp.ithome.com.tw/articles/10276184
+# Bubble Sort
+# O(n²)
 
 data = [89, 34, 23, 78, 67, 100, 66, 29, 79, 55, 78, 88, 92, 96, 96, 23]
 
-# https://ithelp.ithome.com.tw/articles/10202160
-# O(n^2)
+
+def bubble_sort(data):
+    n = len(data)
+    while n > 1:
+        n -= 1
+        for i in range(n):
+            if data[i] > data[i+1]:
+                data[i], data[i+1] = data[i+1], data[i]
+    return data
 
 
-def bubblesort(data):
-    for i in range(len(data)-2):
-        for j in range(len(data)-1):
-            if data[j] > data[j+1]:
-                data[j], data[j+1] = data[j+1], data[j]
-
-
-bubblesort(data)
-print(data)
+print(bubble_sort(data))
