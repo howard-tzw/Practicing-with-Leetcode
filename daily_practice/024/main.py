679cb0a1307f7cf4974b659f77d9b31c81a96f66
Howard <howard.tzw@gmail.com>
2022-11-18 11:20:32 +0800

    day 24 - complete daily algorithm and data structure

---
+++ daily_practice/024/main.py
@@ -0,0 +1,11 @@
+# https://ithelp.ithome.com.tw/articles/10281926
+
+# Divide and conquer
+
+# Dynamic Programming
+
+# Greed Algorithm
+
+# Backtracking
+
+# Branch and bound
--- leetcode/0005.py
+++ leetcode/0005.py
@@ -1,10 +1,31 @@
+# https://leetcode.com/problems/longest-palindromic-substring/
 # https://hackmd.io/@CynthiaChuang/LeetCode-0005-Longest-Palindromic-Substring
 
+# 超難，還沒搞懂...
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if not s:
+            return ""
 
-s = 'abcde'
-for i, char in enumerate(s, start=3):
-    print(i, char)
+        s = "$#" + "#".join(s) + "#@"
+        radii = [0] * len(s)
+        max_right, center = 0, 0
 
-print("#".join(s))
+        for idx, char in enumerate(s[1:-1], start=1):
+            if idx < max_right:
+                radii[idx] = min(
+                    radii[center - (idx - center)], max_right - idx)
 
-print([0] * 5)
+            while s[idx+(radii[idx]+1)] == s[idx-(radii[idx]+1)]:
+                radii[idx] += 1
+
+            if idx + radii[idx] > max_right:
+                max_right = idx + radii[idx]
+                center = idx
+
+        max_radius = max(radii)
+        max_radius_idx = radii.index(max_radius)
+        palindrome = s[max_radius_idx -
+                       max_radius: max_radius_idx+max_radius+1].replace("#", "")
+
+        return palindrome
